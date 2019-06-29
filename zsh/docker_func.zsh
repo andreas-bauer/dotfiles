4fb934fbdbb0bb4674617c5e8023dd1c04f42f77
Andreas Bauer <dev@andreasbauer.org>
2019-06-29 18:40:21 +0200

    Remove dcleanup function because docker prune does the same

--- zsh/docker_func.zsh
+++ zsh/docker_func.zsh
@@ -3,17 +3,6 @@
 #
 # Helper Functions (Source: https://github.com/jessfraz/dotfiles)
 #
-dcleanup(){
-	local containers
-	mapfile -t containers < <(docker ps -aq 2>/dev/null)
-	docker rm "${containers[@]}" 2>/dev/null
-	local volumes
-	mapfile -t volumes < <(docker ps --filter status=exited -q 2>/dev/null)
-	docker rm -v "${volumes[@]}" 2>/dev/null
-	local images
-	mapfile -t images < <(docker images --filter dangling=true -q 2>/dev/null)
-	docker rmi "${images[@]}" 2>/dev/null
-}
 del_stopped(){
 	local name=$1
 	local state
