737e39a66f0fb7c25e06df56aa4bf776a2943910
Andreas Bauer <dev@andreasbauer.org>
2021-02-26 18:08:33 +0100

    Add new aliases for macos

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -36,6 +36,18 @@ alias mount='mount |column -t'
 
 # Important Directories
 alias dev='cd ~/dev'
+alias dl="cd ~/Downloads"
+
+# Get week number
+alias week='date +%V'
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Empty the Trash on all mounted volumes and the main HDD.
+# Also, clear Apple’s System Logs to improve shell startup speed.
+# Finally, clear download history from quarantine. https://mths.be/bum
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
 
 # GIT
 alias gs='git status '
