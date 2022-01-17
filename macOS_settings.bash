b6b67c31be4688c8227d10e8b2b4c67fab6ddfd6
Andreas Bauer <dev@andreasbauer.org>
2022-01-17 16:14:45 +0100

    Extend macOS settings

--- macOS_settings.bash
+++ macOS_settings.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Speed up dock animation
 defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
+# Stop creating .DS_Store files on shared network drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+
