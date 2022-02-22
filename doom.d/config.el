d5601de789e50eba8a76f98a1e4ecd0c4630534c
Andreas Bauer <dev@andreasbauer.org>
2022-02-22 23:22:56 +0100

    Enable referencing BibTex entries

--- doom.d/config.el
+++ doom.d/config.el
@@ -90,3 +90,16 @@
           ("NO"   . +org-todo-cancel)
           ("KILL" . +org-todo-cancel))))
 
+(use-package citar
+  :bind (("C-c b" . citar-insert-citation)
+         :map minibuffer-local-map
+         ("M-b" . citar-insert-preset))
+  :custom
+  (citar-file-open-function 'citar-file-open-external)
+  (citar-bibliography '("~/Library/CloudStorage/OneDrive-BTH/Research Andreas/Literature/zotlib.bib")))
+
+(setq citar-symbols
+      `((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
+        (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
+        (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
+(setq citar-symbol-separator "  ")
