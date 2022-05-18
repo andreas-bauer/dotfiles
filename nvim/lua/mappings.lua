26c582172ffd06c9d34686ff63e9cdfe2987c004
Andreas Bauer <dev@andreasbauer.org>
2022-05-18 10:11:40 +0200

    Change NVIM mappings
    * disable resize with arrow keys

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -26,10 +26,10 @@ map("n", "<S-TAB>", ":bprevious<CR>", default_options)
 map("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_options)
 
 -- Resizing panes
-map("n", "<Left>", ":vertical resize +1<CR>", default_options)
-map("n", "<Right>", ":vertical resize -1<CR>", default_options)
-map("n", "<Up>", ":resize -1<CR>", default_options)
-map("n", "<Down>", ":resize +1<CR>", default_options)
+-- map("n", "<Left>", ":vertical resize +1<CR>", default_options)
+-- map("n", "<Right>", ":vertical resize -1<CR>", default_options)
+-- map("n", "<Up>", ":resize -1<CR>", default_options)
+-- map("n", "<Down>", ":resize +1<CR>", default_options)
 
 -- Autocorrect spelling from previous error
 map("i", "<c-f>", "<c-g>u<Esc>[s1z=`]a<c-g>u", default_options)
