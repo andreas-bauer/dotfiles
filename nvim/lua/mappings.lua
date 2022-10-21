7c5a1e1ceb5186ffaac2ff14081de222a73d1a9d
Andreas Bauer <dev@andreasbauer.org>
2022-10-21 09:43:24 +0200

    Add NVIM mapping to delete without yanking

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -18,6 +18,11 @@ map("v", ">", ">gv", default_options)
 -- paste over currently selected text without yanking it
 map("v", "p", '"_dP', default_options)
 
+-- delete without yanking
+map("n", "<leader>d", '"_d', default_options)
+map("v", "<leader>d", '"_d', default_options)
+map("n", "<leader>dd", '"_dd', default_options)
+
 -- Tab switch buffer
 map("n", "<TAB>", ":bnext<CR>", default_options)
 map("n", "<S-TAB>", ":bprevious<CR>", default_options)
