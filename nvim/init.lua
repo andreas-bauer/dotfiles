fbd1e52a4e24bd90dfc161e258b4e8cdbb75d5e1
Andreas Bauer <dev@andreasbauer.org>
2023-01-03 21:07:30 +0100

    Change NVIM settings because of filtype issue

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,7 @@ require("autocmd")
 -- enable filetypee.lua
 -- This feature is currently opt-in
 -- as it does not yet completely match all of the filetypes covered by filetype.vim
-vim.g.do_filetype_lua = 1
+vim.g.do_filetype_lua = nil
 -- disable filetype.vim
-vim.g.did_load_filetypes = 0
+vim.g.did_load_filetypes = nil
+
