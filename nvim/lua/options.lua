adbd765598de7c04872cd47c66f2cbd7cf7e05b8
Andreas Bauer <dev@andreasbauer.org>
2024-01-31 12:54:52 +0100

    Activate spell check in Neovim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -24,6 +24,8 @@ o.relativenumber = settings.relative_number
 o.cmdheight = 1 -- space for displaying messages/commands
 o.showmode = false -- we don't need to see things like -- INSERT -- anymore
 o.showtabline = 2 -- always show tabs
+o.spelllang = "en_us" -- set spell language
+o.spell = true -- enable spell check
 if settings.global_statusline then
 	o.laststatus = 3
 else
