c71fdf0677e08a4b5db5a5f627edb52da9240f70
Andreas Bauer <dev@andreasbauer.org>
2024-02-11 11:28:00 +0100

    Adjust Neovim telescope files ignore patterns

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
     telescope.setup({
       defaults = {
         path_display = { "truncate " },
-        file_ignore_patterns = { '^node_modules/', },
+        file_ignore_patterns = { "^node_modules/", "^build/", "^dist/" },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
