b37d999cc6a208d7e62de093c332011e66848da6
Andreas Bauer <dev@andreasbauer.org>
2024-02-18 00:09:21 +0100

    Adjust Neovim none-ls config to use eslintrc.json

--- nvim/lua/plugins/lsp/none-ls.lua
+++ nvim/lua/plugins/lsp/none-ls.lua
@@ -44,7 +44,7 @@ return {
         formatting.black,
         diagnostics.eslint_d.with({ -- js/ts linter
           condition = function(utils)
-            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
+            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
           end,
         }),
       },
