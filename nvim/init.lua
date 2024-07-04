5537f0ef59d837c7415847f4ea16b7616b4821b1
Andreas Bauer <dev@andreasbauer.org>
2024-07-04 19:50:47 +0200

    Nvim: disable netrw

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 0
+vim.g.loaded_netrwPlugin = 0
 
 -- References to ./lua/
 -- Vim mappings, see lua/config/which.lua for more mappings
