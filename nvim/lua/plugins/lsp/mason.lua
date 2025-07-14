25633df27a17daa72ab95b5efb2ed9e63315f6d4
Andreas Bauer <dev@andreasbauer.org>
2025-07-14 22:43:42 +0200

    Nvim: Disable the auto install of LSPs via Mason

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -27,15 +27,15 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ts_ls",
-        "eslint",
+        -- "ts_ls",
+        -- "eslint",
         "html",
-        "cssls",
-        "tailwindcss",
+        -- "cssls",
+        -- "tailwindcss",
         "lua_ls",
-        "pyright",
-        "ruff",
-        "gopls",
+        -- "pyright",
+        -- "ruff",
+        -- "gopls",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
@@ -43,11 +43,11 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
+        -- "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "eslint_d", -- js linter
+        -- "isort", -- python formatter
+        -- "black", -- python formatter
+        -- "eslint_d", -- js linter
       },
     })
   end,
