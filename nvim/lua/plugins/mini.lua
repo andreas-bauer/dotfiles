6147768053f39fd46ca309289837213c4318ab80
Andreas Bauer <dev@andreasbauer.org>
2025-08-06 17:43:05 +0200

    Nvim: disable mini.surround

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -7,7 +7,7 @@ return {
   config = function()
     require("mini.ai").setup()
     require("mini.pairs").setup()
-    require("mini.surround").setup()
+    -- require("mini.surround").setup()
     require("mini.icons").setup()
     require("mini.statusline").setup()
     require("mini.tabline").setup()
